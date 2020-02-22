283ced2d4ad0b2dbca081869bc5c2329d157e052
Mateu Cànaves <mateu.canaves@gmail.com>
2020-02-22 17:33:41 +0100

    Scrape categories

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,6 +1,33 @@
+from collections import namedtuple
+from typing import Dict, List
+
 import click
+import httpx
+from parsel import Selector
 
 from scraper.utils import coro
+
+CATEGORIES_URL = (
+    "https://www.poetryfoundation.org/ajax/poems?page=1&sort_by=recently_added"
+)
+
+
+Category = namedtuple("Category", ("name", "slug"))
+
+
+def parse_categories(raw_data: List[Dict]) -> List[Category]:
+    return [Category(c["name"], c["slug"]) for c in raw_data]
+
+
+async def get_poems_categories(ss: httpx.AsyncClient) -> List[Category]:
+    response = await ss.get(CATEGORIES_URL)
+    response_data = response.json()
+    categories_data = next(
+        f["categories"]
+        for f in response_data["Filters"]
+        if f["slug"] == "school-period"
+    )
+    return parse_categories(categories_data)
 
 
 @click.command()
@@ -12,7 +39,8 @@ async def main():
     Simple web scraper that scrapes poems from the Poetry Foundation
     website into a single txt file.
     """
-    pass
+    async with httpx.AsyncClient(http2=True) as client:
+        categories = await get_poems_categories(client)
 
 
 if __name__ == "__main__":
