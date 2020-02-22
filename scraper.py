e2d2614e30fb22427429f957cfd68ca12c96f589
Mateu Cànaves <mateu.canaves@gmail.com>
2020-02-22 12:37:34 +0100

    Initial commit

---
+++ scraper.py
@@ -0,0 +1,16 @@
+import click
+
+
+@click.command()
+def main():
+    """
+    Poetry Foundation Scraper.
+
+    Simple web scraper that scrapes poems from the Poetry Foundation
+    website into a single txt file.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
