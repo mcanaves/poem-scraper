b348d48e5c41ca49309aaaf7c197d6a7a2e7bbee
Mateu Cànaves <mateu.canaves@gmail.com>
2020-02-29 20:17:15 +0100

    Scrape poems lists

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,5 +1,8 @@
+import asyncio
+import logging
 from collections import namedtuple
-from typing import Dict, List
+from math import ceil
+from typing import Dict, List, Optional
 
 import click
 import httpx
@@ -8,9 +11,16 @@ from parsel import Selector
 from scraper.utils import coro
 
 DATA_URL = "https://www.poetryfoundation.org/ajax/poems"
+FILTER_QUERY = "?page={}&school-period={}"
 
+logger = logging.getLogger(__name__)
 
 Category = namedtuple("Category", ("name", "slug"))
+Poem = namedtuple("Poem", ("title", "author", "link"))
+
+
+def parse_poems(raw_data: List[Dict]) -> List[Poem]:
+    return [Poem(p["title"], p["author"], p["link"]) for p in raw_data]
 
 
 def parse_categories(raw_data: List[Dict]) -> List[Category]:
@@ -18,6 +28,7 @@ def parse_categories(raw_data: List[Dict]) -> List[Category]:
 
 
 async def get_poems_categories(ss: httpx.AsyncClient) -> List[Category]:
+    logger.info("Getting poems periods.")
     response = await ss.get(DATA_URL)
     response_data = response.json()
     categories_data = next(
@@ -25,7 +36,30 @@ async def get_poems_categories(ss: httpx.AsyncClient) -> List[Category]:
         for f in response_data["Filters"]
         if f["slug"] == "school-period"
     )
-    return parse_categories(categories_data)
+    categories = parse_categories(categories_data)
+    logger.debug(f"Getted {len(categories)} periods.")
+    return categories
+
+
+async def get_poems_list(
+    ss: httpx.AsyncClient, period: str, page: int = 1
+) -> Optional[List[Dict]]:
+    filter_query = FILTER_QUERY.format(page, period)
+    response = await ss.get(DATA_URL + filter_query)
+    return response.json()
+
+
+async def get_poems(ss: httpx.AsyncClient, period: str) -> List[Poem]:
+    logger.info(f"Getting {period} period poems.")
+    response = await get_poems_list(ss, period)
+    poems = response.get("Entries", [])
+    pages = ceil(response.get("TotalResults", 0) / 20)
+    for page in range(2, pages + 1):
+        response = await get_poems_list(ss, period)
+        poems.extend(response["Entries"])
+    poems = parse_poems(poems)
+    logger.debug(f"Getted {len(poems)} poems of the period {period}.")
+    return poems
 
 
 @click.command()
@@ -37,9 +71,18 @@ async def main():
     Simple web scraper that scrapes poems from the Poetry Foundation
     website into a single txt file.
     """
+    logger.info(f"Start scraping Poetry Fundation.")
     async with httpx.AsyncClient(http2=True) as client:
         categories = await get_poems_categories(client)
+        poems_requests = [get_poems(client, c.slug) for c in categories]
+        await asyncio.gather(*poems_requests)
 
 
 if __name__ == "__main__":
+    formatter = logging.Formatter("[%(levelname)s %(asctime)8s] %(message)s")
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+
     main()
